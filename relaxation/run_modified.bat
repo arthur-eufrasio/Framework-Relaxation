f2e95021dae9bab8b9399e5a1d9b6a59d98c1d2f
eufragod2909 <arthur.eufrasio.ufsc@gmail.com>
2026-02-17 12:33:50 +0100

    fix: correct input file path in run_modified.bat and update job input reference

--- relaxation/run_modified.bat
+++ relaxation/run_modified.bat
@@ -1,8 +1,10 @@
+@echo off
 REM Identifica o diretório onde o script está localizado
 set "current_dir=%~dp0"
 
 REM Caminho completo do arquivo .inp
-set "input_file=%current_dir%backend\files\inp\ImplicitRelaxation_modified.inp"
+set "input_file=%current_dir%backend\files\inp\ImplicitRelaxation.inp"
+set "input_file_modified=%current_dir%backend\files\inp\ImplicitRelaxation_modified.inp"
 set "job_dir=%current_dir%backend\files\job"
 set "job_name=ImplicitRelaxation_modified"
 echo Caminho do arquivo .inp: %input_file%
@@ -15,10 +17,10 @@ if exist "%input_file%" (
     cd /d "%job_dir%"
 
     REM Roda o job
-    abaqus job=%job_name% input="%input_file%"
+    abaqus job=%job_name% input="%input_file_modified%"
 
 ) else (
-    echo O arquivo %input_file% não foi encontrado.
+    echo O arquivo %input_file% nao foi encontrado.
 )
 
 pause
